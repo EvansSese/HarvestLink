e3f9fa27e23d1eeef06bb8facbb25fef813bd2a0
Evans Sese <support@ultimate-tracking.co.ke>
2023-12-05 11:46:45 +0300

    [Fix] SQLAlchemy connection

--- models/engine/storage.py
+++ models/engine/storage.py
@@ -7,13 +7,13 @@ class DatabaseStorage:
     def __init__(self):
         self.db_username = os.getenv("HL_USERNAME")
         self.db_password = os.getenv("HL_PASSWORD")
-        self.db_host = "localhost"
-        self.db_port = "3306"
+        self.db_host = os.getenv("HL_HOST")
+        self.db_port = os.getenv("HL_PORT")
         self.db_name = os.getenv("HL_DATABASE")
         self.mysql_url = (f"mysql+mysqlconnector://"
                           f"{self.db_username}:{self.db_password}@"
                           f"{self.db_host}:{self.db_port}/{self.db_name}")
-        self.engine = create_engine(self.mysql_url, echo=True)
+        self.engine = create_engine(self.mysql_url, pool_pre_ping=True)
         self.Session = sessionmaker(bind=self.engine)
 
     def get_session(self):
